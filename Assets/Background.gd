1de0dfd64bc549282c4f40146e25be9aa8786459
Mark Ribaudo <maribaud@iu.edu>
2020-09-22 16:57:54 -0400

    Completes the game development

--- Assets/Background.gd
+++ Assets/Background.gd
@@ -5,3 +5,7 @@ extends VideoPlayer
 func _on_Background_finished():
 	play()
 
+
+
+func _on_Level_body_entered(body):
+	pass # Replace with function body.
---
+++ Mnu/Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_Play_pressed():
+   get_tree().change_scene("res://Game.tscn")
+
+func _on_Quit_pressed():
+  get_tree().quit()
+
+
+func _on_PLay_pressed():
+	pass # Replace with function body.
